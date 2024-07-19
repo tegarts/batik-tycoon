df557a21edee5cd340dda62ced775de416a058e0
Tegar Nur Hidayat <tegarnurhidayat4@gmail.com>
2024-07-19 23:59:49 +0700

    Worker placing

---
+++ Assets/Scripts/WorkerManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerManager : MonoBehaviour
+{
+    public Machine[] machines;
+    
+    private void Start() 
+    {
+        
+    }
+
+}
