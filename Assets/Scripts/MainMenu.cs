5b242638b4d56d521170b7b07c93ddf2d36afbca
Ahmad Robithoh Akmal <ahmadrobithohakmal@gmail.com>
2024-08-28 19:12:35 +0700

    update_mainmenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,9 +6,28 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameObject popUpLoad;
+    [SerializeField] GameObject panelMainMenu;
+    [SerializeField] GameObject panelOption;
+    [SerializeField] GameObject panelInfo;
+    [SerializeField] Animator animCamera;
+    [SerializeField] Animator animPKanan;
+    [SerializeField] Animator animPKiri;
+    [SerializeField] Animator animFlash;
+
+    AudioSetter audioSetter;
+
+    private void Awake()
+    {
+        audioSetter = GameObject.FindWithTag("Audio").GetComponent<AudioSetter>();
+    }
+
+
     private void Start() 
     {
+        panelMainMenu.SetActive(true);
         popUpLoad.SetActive(false);
+        panelOption.SetActive(false);
+        panelInfo.SetActive(false);
     }
 
     public void ButtonStart()
@@ -26,17 +45,80 @@ public class MainMenu : MonoBehaviour
 
     public void ButtonLoadYes()
     {
-        SceneManager.LoadScene("New Mechanic"); // TODO - Ganti nama scene
+        audioSetter.StopBGM();
+        animCamera.Play("CameraMove");
+        animPKanan.Play("PintuKanan");
+        animPKiri.Play("PintuKiri");
+        audioSetter.PlaySFX(audioSetter.openedDoor);
+        StartCoroutine(SFXLight(3f));
+        animFlash.Play("Flash");
+
+        
+        StartCoroutine(LoadSceneWithDelay(6f)); // 3 detik penundaan
+        panelMainMenu.SetActive(false);
+        popUpLoad.SetActive(false);
     }
 
     public void ButtonLoadNo()
     {
         DataPersistenceManager.instance.NewGame();
-        SceneManager.LoadScene("New Mechanic"); // TODO - Ganti nama scene
+        audioSetter.StopBGM();
+        animCamera.Play("CameraMove");
+        animPKanan.Play("PintuKanan");
+        animPKiri.Play("PintuKiri");
+        audioSetter.PlaySFX(audioSetter.openedDoor);
+        StartCoroutine(SFXLight(3f));
+        animFlash.Play("Flash");
+
+        StartCoroutine(LoadSceneWithDelay(6f)); // 3 detik penundaan
+        panelMainMenu.SetActive(false);
+        popUpLoad.SetActive(false);
     }
 
     public void ButtonLoadExit()
     {
         popUpLoad.SetActive(false);
+        panelMainMenu.SetActive(true);
+    }
+
+    public void ButtonOption()
+    {
+        panelMainMenu.SetActive(false);
+        panelOption.SetActive(true);
+    }
+
+    public void ButtonOptionExit()
+    {
+        panelOption.SetActive(false);
+        panelMainMenu.SetActive(true);
+    }
+
+    public void ButtonInfo()
+    {
+        panelMainMenu.SetActive(false);
+        panelInfo.SetActive(true);
+    }
+
+    public void ButtonInfoExit()
+    {
+        panelInfo.SetActive(false);
+        panelMainMenu.SetActive(true);
+    }
+
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator LoadSceneWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Ganti Scene");
+    }
+
+    private IEnumerator SFXLight(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        audioSetter.PlaySFX(audioSetter.lightSpread);
     }
 }
